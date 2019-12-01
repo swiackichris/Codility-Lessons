b9df2aa6228ba86c408aff911a369df26cbbc1c6
swiackichris <swiacki.chris@gmail.com>
2019-12-01 10:46:41 +0100

    MinAbsSumOfTwo

--- Codility-Lessons/Lesson 15 - Caterpillar method/MinAbsSumOfTwo/MinAbsSumOfTwo/MinAbsSumOfTwo.cpp
+++ Codility-Lessons/Lesson 15 - Caterpillar method/MinAbsSumOfTwo/MinAbsSumOfTwo/MinAbsSumOfTwo.cpp
@@ -1,41 +1,30 @@
-// Fish.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// MinAbsSumOfTwo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
-#include <iostream>
+#include <algorithm>
 
-#include <stack>
-
-int solution(vector<int>& A, vector<int>& B) {
-	// write your code in C++14 (g++ 6.2.0)
-	int N = A.size();
-	int fish = 0;
-	stack<int> ones;
-
-	for (int i = 0; i < N; i++)
-	{
-		if (B[i] == 1)
-		{
-			ones.push(A[i]);
-		}
-
-		while (!ones.empty() && ones.top() < A[i])
-		{
-			ones.pop();
-		}
-
-		if (ones.empty()) fish++;
-	}
-
-	while (!ones.empty())
-	{
-		fish++;
-		ones.pop();
-	}
-
-	return fish;
+int absSort(int i, int j)
+{
+	return abs(i) < abs(j);
 }
 
+int solution(vector<int>& A) {
+	// write your code in C++11 (g++ 4.8.2)
+	int N = A.size();
+	if (N == 1) return 2 * abs(A[0]);
+	sort(A.begin(), A.end(), absSort);
+
+	int minAbs = 2 * abs(A[0]);
+	int currentMin = 2 * abs(A[0]);
+
+	for (int i = 1; i < N; i++)
+	{
+		currentMin = abs(A[i - 1] + A[i]);
+		minAbs = min(currentMin, minAbs);
+	}
+
+	return minAbs;
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
