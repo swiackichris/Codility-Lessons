e974ac6345dabd9edf00d0356e07d2cf421d5ffc
swiackichris <swiacki.chris@gmail.com>
2020-07-04 00:25:30 +0200

    Update FrogJmp.cpp

--- src/FrogJmp.cpp
+++ src/FrogJmp.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int solution(int X, int Y, int D) {
-	// write your code in C++14 (g++ 6.2.0)
+	
 	if ((Y - X) % D > 0) { return (Y - X) / D + 1; }
 	else { return (Y - X) / D; }
 }
