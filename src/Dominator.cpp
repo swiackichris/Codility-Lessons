d90503b57c695761a70e32c36992c9aac41962a1
swiackichris <swiacki.chris@gmail.com>
2020-07-21 23:48:06 +0200

    Update Dominator.cpp

--- src/Dominator.cpp
+++ src/Dominator.cpp
@@ -1,61 +1,29 @@
-// Dominator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
-#include <iostream>
-
 #include <algorithm>
 
 int solution(vector<int> &A) {
-	// write your code in C++14 (g++ 6.2.0)
-	int N = A.size();
-	vector<int> copy(N);
-	int dominatorCount = 1;
-	bool dominator = false;
-	int dominatorNumber = 0;
-
-	for (int i = 0; i < N; i++)
-	{
-		copy[i] = A[i];
-	}
-
-	sort(copy.begin(), copy.end());
-
-	for (int i = 0; i < N; i++)
-	{
-		if (copy[i] == copy[i + 1])
-		{
-			dominatorCount++;
-		}
-		if (dominatorCount > N / 2)
-		{
-			dominatorNumber = copy[i];
-			dominator = true;
-			break;
-		}
-		else if (copy[i] != copy[i + 1])
-		{
-			dominatorCount = 1;
-		}
-	}
-
-	for (int i = 0; i < N; i++)
-	{
-		if (A[i] == dominatorNumber && dominator)
-		{
-			return i;
-		}
-	}
-	return -1;
+    // write your code in C++14 (g++ 6.2.0)
+    if(A.empty()) return -1;
+    if(A.size() == 1) return 0;
+    vector<int>B(A);
+    sort(A.begin(), A.end());
+    int count = 1;
+    
+    for(int i = 1; i < A.size(); i++)
+    {
+        if(A[i] == A[i-1]) 
+        {
+            count++;
+        }
+        else count = 1;
+        if(count*2>A.size()) 
+        {
+            int dominator = A[i];
+            for(int i = 0; i < B.size(); i++)
+            {
+                if(B[i] == dominator) return i;
+            }
+        }
+    }
+    
+    return -1;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
